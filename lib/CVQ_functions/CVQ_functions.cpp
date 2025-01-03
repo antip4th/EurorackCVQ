8d897daa654a325b367445e70702cc5738db0e7f
Raphael Sauer <raphael.sauer@recalm.com>
2025-01-03 12:24:25 +0100

    removed incorrect calibration function, modified LED-Blink

--- lib/CVQ_functions/CVQ_functions.cpp
+++ lib/CVQ_functions/CVQ_functions.cpp
@@ -3,16 +3,16 @@
 #include "DAC7565.h"
 
 
-void blinkInternalLED(uint8_t blinks, bool ledState)
+void blinkInternalLED(uint8_t blinks, bool ledState, uint16_t msDelay)
 {
     if(ledState == HIGH)
     {
         for (size_t i = 0; i < blinks; i++)
         {
         digitalWrite(LED_BUILTIN,LOW);
-        sleep_ms(200);
+        sleep_ms(msDelay);
         digitalWrite(LED_BUILTIN,HIGH);
-        sleep_ms(200);
+        sleep_ms(msDelay);
         }
     }
     if(ledState == LOW)
@@ -20,72 +20,37 @@ void blinkInternalLED(uint8_t blinks, bool ledState)
         for (size_t i = 0; i < blinks; i++)
         {
         digitalWrite(LED_BUILTIN, HIGH);
-        sleep_ms(200);
+        sleep_ms(msDelay);
         digitalWrite(LED_BUILTIN, LOW);
-        sleep_ms(200);
+        sleep_ms(msDelay);
         }
     }
 }
 
-int16_t calibrateOffset(uint8_t calibrationConfig[], uint8_t configLen, MCP346xR *adc, DAC *dac)
+void waitForPinAction(uint8_t pin, PinStatus polarity, uint16_t msSleep)
 {
-    int16_t offsetErrorVal = 0;
-    uint16_t numSamples = 1000; 
-    
-    // Wait for "0V at input" button press
-    while(digitalRead(PUSH_BUTTON_PIN))
+    if(polarity == FALLING)
     {
-        sleep_ms(100);
-    }
-       
-    // set ADC to single conv mode
-    adc->writeConfig(calibrationConfig, configLen);
-    adc->startConversion();
-    
-    // take many measurements
-    for (size_t i = 0; i < numSamples; i++)
-    {
-        while(!adc->getDataReady()){
-            sleep_ms(1);
+        while(digitalRead(pin))
+        {
+            sleep_ms(msSleep);
         }
-        offsetErrorVal += adc->readValue();
-        Serial1.println(offsetErrorVal);
-        adc->startConversion();
-    }
-    // average over these measurements
-    offsetErrorVal = static_cast<int16_t>(round(offsetErrorVal/numSamples));
-    
-    Serial1.print("Input Offset Value: ");
-    Serial1.print(offsetErrorVal);
-    Serial1.println("LSB");
-
-    // send value to output
-    dac->writeChannel(DAC_CHANNEL_A, CONVERTERS_FULL_SCALE - offsetErrorVal<<1);
-    // signal 0V measurement finished
-    blinkInternalLED(2, 1);
-
-    // Wait for "output at input" button press
-    while(digitalRead(PUSH_BUTTON_PIN))
-    {
-        sleep_ms(100);
-    }
-
-    // take many measurements
-    for (size_t i = 0; i < numSamples; i++)
-    {
-        while(!adc->getDataReady()){
-            sleep_ms(1);
+        do
+        {
+            sleep_ms(msSleep);
+        } while(!digitalRead(pin));
+    } else 
+    {   
+        if (polarity == RISING)
+        {
+            while(!digitalRead(pin))
+            {
+                sleep_ms(msSleep);
+            }
+            do
+            {
+                sleep_ms(msSleep);
+            } while(digitalRead(pin));
         }
-        offsetErrorVal += adc->readValue();
-        adc->startConversion();
     }
-
-    // average over these measurements
-    offsetErrorVal /= numSamples;
-    
-    return -offsetErrorVal;
-}
-
-void calibrateGain()
-{
 }
--- lib/CVQ_functions/CVQ_functions.h
+++ lib/CVQ_functions/CVQ_functions.h
@@ -3,12 +3,6 @@
 #include "MCP346xR.h"
 #include "../../include/defines.h"
 
-void blinkInternalLED(uint8_t blinks, bool ledState);
+void blinkInternalLED(uint8_t blinks, bool ledState, uint16_t msDelay=200);
 
-uint16_t external2internal(uint16_t externVal);
-
-uint16_t internal2external(uint16_t internVal);
-
-int16_t calibrateOffset(uint8_t calibrationConfig[], uint8_t configLen, MCP346xR *adc,DAC *dac);
-
-void calibrateGain();
+void waitForPinAction(uint8_t pin, PinStatus polarity, uint16_t msSleep);
